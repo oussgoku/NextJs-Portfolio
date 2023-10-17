70e17caf9de39b01682bae693e7f55160eff1a86
oussgoku <ouss.gharbii@gmail.com>
2023-10-17 20:48:58 +0200

    Hero Section creation

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+     <h1 className='text-white'>Portfolio website</h1>
+     <p className="text-gray">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis possimus reiciendis facilis, aliquid rem aperiam illum dicta! Error harum numquam laborum illo libero dolorem aliquid, quidem, consectetur ipsam eos similique!
+        
+     </p>
+    </section>
+  );
+};
+
+export default HeroSection;
